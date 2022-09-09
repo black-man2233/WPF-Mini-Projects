3d34b78feaf3b582b3ceadad2a7769f0240f13c8
Simp-Lord <kevi3758@gmail.com>
2022-09-09 11:59:05 +0200

    Startup on new Project about OOP

---
+++ ClassAct/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ClassAct
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        MyClass myClass = null;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Create_btn(object sender, RoutedEventArgs e)
+        {
+            myClass = new MyClass();
+            myClass.Navn = "Kevin";
+            Text_Box.Text = myClass.Navn;
+        }
+
+        private void Destroy_btn(object sender, RoutedEventArgs e)
+        {
+            myClass = null;
+            GC.Collect();
+
+        }
+    }
+}
---
+++ ClassAct/MyClass.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ClassAct 
+{
+    class MyClass : Window
+
+    {
+        //int Tal;
+        public string Navn;
+         
+        public MyClass()
+        {
+
+            Debug.Write("Dette er Constructoren! \n");
+            
+        }
+        ~MyClass()
+        {
+            if (Navn == null)
+            {
+                Debug.Write("Hej " + Navn + ", dette er Destructoren! \n");
+
+            }
+
+                GC.Collect();
+
+        }
+    }
+
+}
