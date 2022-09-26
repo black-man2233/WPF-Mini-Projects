189eefe96517f56ef24bd5f60ed4b1e39bf6c750
Kevin Bamwesa Ishara <kevi3758@elev.techcollege.dk>
2022-09-26 12:51:59 +0200

    Two Ways, dindings

---
+++ TwoWaysTest/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TwoWayTest;
+
+namespace TwoWaysTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<Person> Personer = new ObservableCollection<Person>();
+        Person person = new Person(12, "Svend", "Bendt", 123);
+
+        //Main Window 
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Personer.Add(person);
+            Personer.Add(new Person(0, "Bein", "Stagge", -987654321));
+            Personer.Add(new Person(1, "Turt", "Khorsen", 0));
+            Personer.Add(new Person(2, "Gill", "Bates", int.MaxValue));
+
+
+            this.DataContext = Personer;
+        }
+
+        //Vis Data button
+        private void btnVisData_Click(object sender, RoutedEventArgs e)
+        {
+            string PersonData = $"{person.Fornavn} {person.Efternavn} har en formue på {person.Formue} Kr.";
+
+            MessageBox.Show(PersonData);
+        }
+
+        //Update button
+        private void btnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            person.Formue++;
+        }
+    }
+}
---
+++ TwoWaysTest/Person.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel; // Tilføjes i toppen af din C# fil!
+using System.Runtime.CompilerServices;
+
+namespace TwoWayTest
+{
+
+    class Person : INotifyPropertyChanged
+    {
+        public int ID;
+        //fornanv properti
+        private string _fornavn;
+        public string Fornavn
+        {
+            get
+            {
+                return _fornavn;
+            }
+            set
+            {
+                _fornavn = value;
+                OnPropertyChanged("Fornavn");
+            }
+        }
+        //Eftervnan properti
+        private string _efternavn;
+        public string Efternavn
+        {
+            get
+            {
+                return _efternavn;
+            }
+            set
+            {
+                _efternavn = value;
+                OnPropertyChanged("Efternavn");
+
+            }
+        }
+        //formue properti
+        private int _formue;
+        public int Formue
+        {
+            get
+            {
+                return _formue;
+            }
+            set
+            {
+                _formue = value;
+                OnPropertyChanged("Formue");
+            }
+        }
+
+        public Person(int ID, string Fornavn, string Efternavn, int Formue)
+        {
+            this.ID = ID;
+            this.Fornavn = Fornavn;
+            this.Efternavn = Efternavn;
+            this.Formue = Formue;
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string PropertyNavn)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(PropertyNavn));
+            }
+        }
+    }
+
+
+
+}
